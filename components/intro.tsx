744857daf410b895bed3dc60050f630ed3c3648b
JackMulcunry <jack.mulcunry@gmail.com>
2026-02-04 14:47:33 -0500

    Assignment 2: Added h1 header, paragraph, unordered list with one list element

---
+++ components/intro.tsx
@@ -0,0 +1,8 @@
+
+export default function Intro() {
+  return (
+    <div>
+        <h1>Hello Student Name</h1>
+    </div>
+  );
+}
---
+++ components/unordered.tsx
@@ -0,0 +1,10 @@
+
+export default function Unordered() {
+  return (
+    <div>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipiscing elit.</li>
+        </ul>
+    </div>
+  );
+}
